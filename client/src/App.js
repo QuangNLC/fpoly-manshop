b7b57f4bdeb31a8edf119c2cb692d2c66b8e2b75
Quang NguyenIch NLC <work.quangni1211@gmail.com>
2022-10-04 17:12:46 +0700

    merge code feature login/register tempalte

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import WebsiteLayout from './layout/WebsiteLayout';
-import WebHomePage from './pages/website/WebHomePage';
-import WebProductList from './pages/website/WebProductList';
-import WebContact from './pages/website/WebContact';
-import WebAboutUs from './pages/website/WebAboutUs';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WebsiteLayout from "./layout/WebsiteLayout";
+import WebHomePage from "./pages/website/WebHomePage";
+import WebProductList from "./pages/website/WebProductList";
+import WebContact from "./pages/website/WebContact";
+import WebAboutUs from "./pages/website/WebAboutUs";
+import Login from "./pages/website/Login";
+import Register from "./pages/website/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<WebsiteLayout />}>
+        <Route path="/" element={<WebsiteLayout />}>
           <Route index element={<WebHomePage />} />
           <Route path="products" element={<WebProductList />} />
           <Route path="about-us" element={<WebAboutUs />} />
           <Route path="contact" element={<WebContact />} />
-          <Route path="*" element={<div>Trang khong ton tai</div>}/>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="*" element={<div>Trang khong ton tai</div>} />
         </Route>
       </Routes>
     </BrowserRouter>
